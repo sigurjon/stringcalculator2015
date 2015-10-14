a3bc2bdad1c0e23eacbe9c121507aee0475ec065
sigurjon <sigurjonb11@ru.is>
2015-10-14 21:46:09 +0000

    added first 3 test

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,18 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		return 0;
+		if (text.equals(""))
+			return 0;
+
+		else if(text.contains(",")){
+			String [] numbers = text.split(","); 
+			return toInt(numbers[0]) + toInt(numbers[1]);
+		}
+
+		else
+			return 1;
+	}
+	private static int toInt(String numbers){
+		return  Integer.parseInt(numbers);
 	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -13,4 +13,14 @@ public class CalculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumber() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
 }
