ad437909a6896638a24d2167255d2840cc1dc046
sigurjon <sigurjonb11@ru.is>
2015-10-17 09:43:31 +0000

    add main forloop for numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,18 +3,25 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		if (text.equals(""))
+		if (text.equals("")){
 			return 0;
+		}
 
+		
 		else if(text.contains(",")){
-			String [] numbers = text.split(","); 
-			return toInt(numbers[0]) + toInt(numbers[1]);
+			return sum(splitNumbers(text));
 		}
 
 		else
 			return 1;
 	}
-	private static int toInt(String numbers){
-		return  Integer.parseInt(numbers);
+	private static int toInt(String number){
+		return  Integer.parseInt(number);
 	}
-}
+}  private static int sum(String[] numbers){
+ 	    int total = 0;
+        for(String number : numbers){
+		    total += toInt(number);
+		}
+		return total;
+    }
